76a47c96b496667922a4bd6e4af4c9c6cf203b19
alpharush <0xalpharush@protonmail.com>
2024-06-05 15:53:43 -0500

    init

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,8 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
-contract Counter {
-    uint256 public number;
+import { IERC721Receiver } from "@oz/interfaces/IERC721Receiver.sol";
+import { IERC1155Receiver } from "@oz/interfaces/IERC1155Receiver.sol";
+import { IERC165 } from "@oz/utils/introspection/IERC165.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
-    }
-}
+// solhint-disable-next-line no-empty-blocks
+interface IFallbackHandler is IERC165, IERC721Receiver, IERC1155Receiver {}
